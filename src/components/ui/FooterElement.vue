4e46507b0055ab1cba0f223d7610663f6442966f
Daniel Sisovic <d.sisovic777@gmail.com>
2025-02-16 17:55:16 +0100

    Update footer for tablet view

--- src/components/ui/FooterElement.vue
+++ src/components/ui/FooterElement.vue
@@ -3,7 +3,7 @@
     class="w-full bg-[var(--grey-900)] rounded-t-md rounded-b-none pt-2 px-4 pb-0 h-[3.25rem] flex justify-around items-center tablet:h-[4.625rem] tablet:px-10"
   >
     <div
-      class="pt-2 pb-3 px-6 rounded-t-md border-b-4 border-[var(--green)] bg-[var(--beige-100)] tablet:h-[4.125rem] tablet:w-[6.5rem] tablet:inline-flex tablet:items-center tablet:justify-center tablet:flex-col tablet:gap-[0.438rem]"
+      class="pt-2 pb-3 px-6 rounded-t-md border-b-4 border-[var(--green)] bg-[var(--beige-100)] tablet:h-[4.125rem] tablet:w-[6.5rem] tablet:inline-flex tablet:items-center tablet:space-around tablet:flex-col"
     >
       <img
         src="@/assets/images/icon-nav-overview.svg"
@@ -12,13 +12,13 @@
       />
 
       <span
-        class="hidden leading-[150%] font-bold text-[0.75rem] tablet:block whitespace-nowra text-[var(--grey-900)]"
+        class="hidden leading-[150%] font-bold text-[0.75rem] tablet:block mt-[0.438rem] whitespace-nowrap text-[var(--grey-900)]"
         >Overview</span
       >
     </div>
 
     <div
-      class="pt-2 pb-3 px-6 tablet:h-[4.125rem] tablet:w-[6.5rem] tablet:inline-flex tablet:items-center tablet:justify-center tablet:flex-col tablet:gap-[0.438rem]"
+      class="pt-2 pb-3 px-6 tablet:h-[4.125rem] tablet:w-[6.5rem] tablet:inline-flex tablet:items-center tablet:space-around tablet:flex-col"
     >
       <img
         src="@/assets/images/icon-nav-transactions.svg"
@@ -27,35 +27,35 @@
       />
 
       <span
-        class="hidden leading-[150%] font-bold text-[0.75rem] tablet:block whitespace-nowra text-[var(--grey-300)]"
+        class="hidden leading-[150%] font-bold text-[0.75rem] tablet:block mt-[0.438rem] whitespace-nowrap text-[var(--grey-300)]"
         >Transactions</span
       >
     </div>
 
     <div
-      class="pt-2 pb-3 px-6 tablet:h-[4.125rem] tablet:w-[6.5rem] tablet:inline-flex tablet:items-center tablet:justify-center tablet:flex-col tablet:gap-[0.438rem]"
+      class="pt-2 pb-3 px-6 tablet:h-[4.125rem] tablet:w-[6.5rem] tablet:inline-flex tablet:items-center tablet:space-around tablet:flex-col"
     >
       <img src="@/assets/images/icon-nav-budgets.svg" alt="budgets" class="tablet:w-6 tablet:h-6" />
 
       <span
-        class="hidden leading-[150%] font-bold text-[0.75rem] tablet:block whitespace-nowra text-[var(--grey-300)]"
+        class="hidden leading-[150%] font-bold text-[0.75rem] tablet:block mt-[0.438rem] whitespace-nowrap text-[var(--grey-300)]"
         >Budgets</span
       >
     </div>
 
     <div
-      class="pt-2 pb-3 px-6 tablet:h-[4.125rem] tablet:w-[6.5rem] tablet:inline-flex tablet:items-center tablet:justify-center tablet:flex-col tablet:gap-[0.438rem]"
+      class="pt-2 pb-3 px-6 tablet:h-[4.125rem] tablet:w-[6.5rem] tablet:inline-flex tablet:items-center tablet:space-around tablet:flex-col"
     >
       <img src="@/assets/images/icon-nav-pots.svg" alt="pots" class="tablet:w-6 tablet:h-6" />
 
       <span
-        class="hidden leading-[150%] font-bold text-[0.75rem] tablet:block whitespace-nowra text-[var(--grey-300)]"
+        class="hidden leading-[150%] font-bold text-[0.75rem] tablet:block mt-[0.438rem] whitespace-nowrap text-[var(--grey-300)]"
         >Pots</span
       >
     </div>
 
     <div
-      class="pt-2 pb-3 px-6 tablet:h-[4.125rem] tablet:w-[6.5rem] tablet:inline-flex tablet:items-center tablet:justify-center tablet:flex-col tablet:gap-[0.438rem]"
+      class="pt-2 pb-3 px-6 tablet:h-[4.125rem] tablet:w-[6.5rem] tablet:inline-flex tablet:items-center tablet:space-around tablet:flex-col"
     >
       <img
         src="@/assets/images/icon-nav-recurring-bills.svg"
@@ -64,7 +64,7 @@
       />
 
       <span
-        class="hidden leading-[150%] font-bold text-[0.75rem] text-[var(--grey-300)] whitespace-nowrap tablet:block"
+        class="hidden leading-[150%] font-bold text-[0.75rem] text-[var(--grey-300)] mt-[0.438rem] whitespace-nowrap tablet:block"
       >
         Recurring bills</span
       >
